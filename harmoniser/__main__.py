bc8d7c85fafb9338a2c52cf953fa244d25e09e7c
Curtis Hu <curtis.hu688@gmail.com>
2024-04-20 23:34:40 -0700

    stuff 2

--- harmoniser/__main__.py
+++ harmoniser/__main__.py
@@ -7,11 +7,19 @@ from pynput import keyboard
 
 
 p = pyaudio.PyAudio()
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+
+for i in range(0, numdevices):
+    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
 stream = p.open(format=pyaudio.paFloat32,
                 channels=1 if sys.platform == 'darwin' else 2,
                 rate=SAMPLE_RATE,
                 input=True,
                 output=True,
+                input_device_index=2,
                 frames_per_buffer=CHUNK)
 # listener = keyboard.Listener(
 #     on_press=on_press)
@@ -30,8 +38,15 @@ while True:
     buffer = np.frombuffer(in_buffer, dtype=np.float32) # as array of floats
 
     # AUDIO PROCESSING HERE 
-    # buffer = harmoniser(buffer)
+
+    # buffer = major_chord(buffer)
     # buffer = frog(buffer)
+    # buffer = low_voice(buffer)
+    # buffer = high_voice(buffer)
+    # buffer = darth_vader(buffer)
+    # buffer = low_pass(buffer)
+    buffer = low_pass(buffer)
+
     # AUDIO PROCESSING ENDS HERE 
 
     out_buffer = buffer.tobytes()
--- harmoniser/effects.py
+++ harmoniser/effects.py
@@ -4,12 +4,48 @@ import librosa
 from .constants import *
 import numpy as np
 
-def harmoniser(chunk):
+def harmoniser(buffer):
 	# read the keyboard input
 	# pitch shift into those midi
-	chunk = librosa.effects.pitch_shift(chunk, sr=SAMPLE_RATE, n_steps=4)
-	return chunk
-	
+	buffer = librosa.effects.pitch_shift(buffer, sr=SAMPLE_RATE, n_steps=4)
+	return buffer
+
+def major_chord(buffer):
+	fundamental = buffer
+	major_third = librosa.effects.pitch_shift(buffer, sr=SAMPLE_RATE, n_steps=4)
+	fifth = librosa.effects.pitch_shift(buffer, sr=SAMPLE_RATE, n_steps=7)
+	return fundamental + major_third + fifth
+
+def minor_chord(buffer):
+	fundamental = buffer
+	minor_third = librosa.effects.pitch_shift(buffer, sr=SAMPLE_RATE, n_steps=3)
+	fifth = librosa.effects.pitch_shift(buffer, sr=SAMPLE_RATE, n_steps=7)
+	return fundamental + minor_third + fifth
+
+def fifth(buffer):
+	buffer = librosa.effects.pitch_shift(buffer, sr=SAMPLE_RATE, n_steps=7)
+	return buffer
+
+def low_voice(buffer):
+	buffer = librosa.effects.pitch_shift(buffer, sr=SAMPLE_RATE, n_steps=-7)
+	return buffer
+
+def high_voice(buffer):
+	buffer = librosa.effects.pitch_shift(buffer, sr=SAMPLE_RATE, n_steps=12)
+	return buffer
+
+def doppler_effect(buffer):
+	buffer = librosa.effects.pitch_shift(buffer, sr=SAMPLE_RATE, n_steps=-1)
+	return buffer
+
+def darth_vader(buffer):
+	buffer = low_voice(buffer)
+	buffer = frog(buffer)
+	return buffer
+
+def batman(buffer):
+	buffer = low_voice(buffer)
+	return buffer
 
 def frog(buffer):
 	freqs = librosa.stft(buffer)
@@ -22,6 +58,64 @@ def frog(buffer):
 
 	buffer = librosa.istft(freqs)
 	return buffer
+
+def low_pass(buffer, f=1000):
+	freqs = librosa.stft(buffer)
+
+	# cutoff = np.linspace(1, , num=freqs.shape[0], base=2.0)
+	# cutoff = [1/(1 + np.exp(x)) for x in range(freqs.shape[0])]
+	cutoff = np.ones(freqs.shape[0])
+	A = np.abs(freqs)
+
+	# multiply across
+	B = (A.T * cutoff).T 
+
+	phase = np.angle(freqs)
+
+	freqs = A*np.exp(phase * 1j)
+
+	buffer = librosa.istft(freqs)
+	return buffer
+
+def high_pass(buffer):
+	freqs = librosa.stft(buffer)
+
+	# cutoff = np.linspace(1, , num=freqs.shape[0], base=2.0)
+	# cutoff = [1/(1 + np.exp(x)) for x in range(freqs.shape[0])]
+	cutoff = np.ones(freqs.shape[0])
+	A = np.abs(freqs)
+
+	# multiply across
+	B = (A.T * cutoff).T 
+
+	phase = np.angle(freqs)
+	freqs = A*np.exp(phase * 1j)
+
+	buffer = librosa.istft(freqs)
+	return buffer
+
+def old_phonograph(buffer):
+	freqs = librosa.stft(buffer)
+	A = np.abs(freqs)
+
+	phase = np.angle(freqs)
+
+	freqs = np.tanh(A*np.exp(phase * 1j))
+
+	buffer = librosa.istft(freqs)
+	return buffer
+
+def distortion(buffer, gain=3):
+	freqs = librosa.stft(buffer)
+	A = np.abs(freqs)
+
+	phase = np.angle(freqs)
+
+	freqs = np.tanh(gain*A*np.exp(phase * 1j))
+
+	buffer = librosa.istft(freqs)
+	return buffer
+
 
 
 def separate(A, phase):
